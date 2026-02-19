c527f6c83f9f50dca5ac39225d0f7bcfb1ccf4d3
Anonymous Maarten <anonymous.maarten@gmail.com>
2026-02-19 21:27:53 +0100

    Remove MSVC-only Exception Handler from ThreadClass API

--- Code/Commando/bandwidthcheck.h
+++ Code/Commando/bandwidthcheck.h
@@ -100,7 +100,7 @@ class BandwidthCheckerClass
 
 		static class BandwidthCheckerThreadClass : public ThreadClass {
 			public:
-				BandwidthCheckerThreadClass(const char *thread_name = "Bandwidth checker thread") : ThreadClass(thread_name, &Exception_Handler) {}
+				BandwidthCheckerThreadClass(const char *thread_name = "Bandwidth checker thread") : ThreadClass(thread_name) {}
 				void Thread_Function(void) override {BandwidthCheckerClass::Check();};
 		} Thread;
 		friend BandwidthCheckerThreadClass;
